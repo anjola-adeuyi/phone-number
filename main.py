fb5e74a6009dd2f235325babf7dbca5595a83773
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-07-12 21:13:07 +0100

    phone number formats national

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ import phonenumbers
 # pip3 install phonenumbers
 
 from test import number
+from phonenumbers import geocoder
+
+ch_number = phonenumbers.parse(number, "NG")
+print(phonenumbers.format_number(ch_number, phonenumbers.PhoneNumberFormat.E164))
+print(phonenumbers.format_number(ch_number, phonenumbers.PhoneNumberFormat.INTERNATIONAL))
+print(phonenumbers.format_number(ch_number, phonenumbers.PhoneNumberFormat.NATIONAL))
+print(phonenumbers.format_number(ch_number, phonenumbers.PhoneNumberFormat.RFC3966))
+print(geocoder.description_for_number(ch_number, "en"))
